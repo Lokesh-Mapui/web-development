b49292a9f3c27bc54a7681146691b8932161eed8
Lokesh Mapui <lokeshmapui88@gmail.com>
2025-04-06 14:15:36 +0530

    basic javascript

---
+++ javascript/js_class_1/index.js
@@ -0,0 +1,54 @@
+console.log('namaste duniya version 5');
+
+// var a=true;
+// var a=false;
+// console.log(a);
+
+// const b=5;
+// console.log(b); 
+
+// let lastname='babbar';
+// console.log(lastname);
+ 
+// lastname=3;
+// console.log(lastname);  
+
+// console.log(5===3)
+// let age=57;
+// let ostatus =(age>=18) ? 'yes vote':'can not vote';
+// console.log(ostatus);
+
+// let marks=90;
+// if(marks>=90){
+//     console.log('A');
+// }
+// else if(marks>=80){
+//     console.log('B');
+// }
+// else if(marks>=70){
+//     console.log('C');
+// }
+// else{
+//     console.log('D');
+// }
+
+// let num=3;
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     default:console.log('C');
+// }
+
+for(let i=0;i<5;i++){  
+    console.log(i); 
+}
+
+let x=5;
+while(x<=5){
+    console.log(x);
+    x++;
+}
