49b7ee8a7af6e562d1b2d000243c4d86e3fb7538
Lokesh Mapui <lokeshmapui88@gmail.com>
2025-06-19 13:47:00 +0530

    class 1

---
+++ javascript/shradha khapra/variable_and_datatypes_class1/first.js
@@ -0,0 +1,46 @@
+// nam="lokesh mapui";
+// console.log(nam);
+// age=20;
+// console.log(age);
+// x=null;
+// console.log(x)
+// y=undefined;
+// console.log(y);
+
+// var fullname="lokesh mapui";
+// console.log(fullname);
+
+// var fullname="love babbar";
+// console.log(fullname);
+
+// let age=39;
+// console.log(age+1);
+
+// console.log("love babbar");
+// alert("love babbar wants to meet you");
+// console.log("i love js");
+
+// const student={
+//     fullname: "lokesh mapui",
+//     age:20,
+//     cgpa:7.5,
+//     ispass:true
+// };
+//  student["age"]=student["age"]+1;
+//  console.log(student["age"]);
+
+const product={
+    title:"Ball Pen",
+    rating:4,
+    offer:5,
+    price:270,
+};
+
+const profile={
+    title:"@shradhakhapra",
+    isFollow:false,
+    posts:195,
+    followers:569000,
+    following:4,
+};
+console.log(typeof profile.isFollow);
