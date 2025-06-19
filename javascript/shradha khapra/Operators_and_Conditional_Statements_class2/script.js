29c5ecdeca96b2b026be545ef8176d135beb9956
Lokesh Mapui <lokeshmapui88@gmail.com>
2025-06-19 13:49:53 +0530

    class2

---
+++ javascript/shradha khapra/Operators_and_Conditional_Statements_class2/script.js
@@ -0,0 +1,33 @@
+// arithmetic operators
+// let a=5;
+// let b=2;
+
+// console.log("a + b =", a +b);
+// console.log("a - b =", a-b);
+// console.log("a * b =", a *b);
+// console.log("a / b =", a/b);
+// console.log("a % b =", a%b);
+// console.log("a ** b =", a**b);
+
+// unary operator
+// let a=5;
+// let b=2;
+// console.log("a  =", a," &  b =", b);
+// a = --a;
+// console.log("a =", a);
+
+// assignment operator
+// let a=5;
+// let b=2;
+// console.log("a  =", a," &  b =", b);
+// a+=4
+// console.log("a =", a);
+// a**=4
+// console.log("a =", a);
+
+// logical operators
+// let a=6;
+// let b=5;
+// let cond1=a>b; //true
+// let cond2=a==6; //true
+// console.log("cond1& & cond2 =",cond1||cond2);
