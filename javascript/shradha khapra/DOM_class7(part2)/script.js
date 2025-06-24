e44ff9c7a289beeb361859876e4d3dedec5c16d7
Lokesh Mapui <lokeshmapui88@gmail.com>
2025-06-24 11:07:55 +0530

    part2

---
+++ javascript/shradha khapra/DOM_class7(part2)/script.js
@@ -0,0 +1,47 @@
+// let div= document.querySelector("div");
+// console.log(div);
+
+// const { createElement } = require("react");
+
+// let name=div.getAttribute("name");
+// console.log(name);
+
+
+// let para=document.querySelector("p");
+// console.log(para);
+
+// let paragraph = para.setAttribute("class","newclass");
+// console.log(paragraph);
+
+// let div= document.querySelector("div");
+// console.log(div);
+
+// div.style.backgroundColor="red";  
+
+// let newbtn=document.createElement("button");
+// newbtn.innerText = "click me!";
+
+// let p= document.querySelector("p");
+// p.after(newbtn);
+// console.log(div);
+
+// let newheading= document.createElement("h1");
+// newheading.innerHTML = "<i> hi, i am new here </i>";
+// document.querySelector("body").prepend(newheading);
+// let para =document.querySelector("p");
+// para.remove();
+
+// Q-1)
+
+// let newbtn =document.createElement("button");
+// newbtn.innerText ="click me!";
+// newbtn.style.backgroundColor="red";
+// newbtn.style.color="white";
+
+// let bodytag =document.querySelector("body");
+// bodytag.prepend(newbtn);
+
+// Q-2)
+
+let paragraph =document.querySelector("p");
+paragraph.classList.add("newclass");
