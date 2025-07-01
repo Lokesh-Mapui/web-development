d989b7c24332b977355ad677eddb6d14d64ff022
Lokesh Mapui <lokeshmapui88@gmail.com>
2025-07-01 10:56:37 +0530

    class3

---
+++ javascript/love babbar/js_class_3/script.js
@@ -0,0 +1,66 @@
+
+// let courses =[
+//     {no:1, naam :'love'},
+//     {no:2, naam:'rahul'}
+// ];
+
+// let course=courses.find((course)=>{
+//     return course.naam ==='love';
+// })
+// console.log(course);
+
+
+// let numbers=[1,2,3,4,5];
+// let numbers2=numbers;
+// numbers=[];
+
+// numbers.length=0;
+// numbers.splice(0,numbers.length);
+// console.log(numbers)
+// console.log(numbers2)
+
+// let first =[2,3,4,5];
+// let second=[6,7,8,9];
+// let combine=first.concat(second);
+// console.log(combine);
+
+// let arr=[23,45,65,21,6,1];
+// let piece=arr.slice(1,4);
+// console.log(piece);
+
+
+// let first =[2,3,4,5];
+// let second=[6,7,8,9];
+// let combined = [...first,...second];
+// console.log(combined);
+//  let another=[...combined];
+
+
+//  let arr=[10,20,30,40,50];
+//  arr.forEach ((number)=>{ 
+//     console.log(number);
+//  })
+
+// let numbers=[10,20,30,40,50];
+// const joined= numbers.join(',')
+// console.log(joined);
+
+// let message='This is my first message';
+// let parts=message.split(' ');
+// console.log(parts);
+
+// let joined= parts.join('_')
+// console.log(joined);
+
+// let numbers=[40,30,10,20,50];
+// let sor=numbers.sort();
+// console.log(sor);
+// let reverse= numbers.reverse();
+// console.log(reverse);
+
+// let numbers=[7,8,9,10];
+// let items=numbers.map(function(value){
+//     return 'student_no' +value;
+// });
+
+// console.log(items);
