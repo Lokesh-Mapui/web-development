f9abc5805a635b567fc80cc3456ec9c9535df63b
Lokesh Mapui <lokeshmapui88@gmail.com>
2026-01-09 12:50:10 +0530

    learn about datatypes

---
+++ Hitesh Chowdhary/JS_foundation/part2/nonPrimitve.js
@@ -0,0 +1,25 @@
+const username = {
+    firstname : "Lokesh", isloggedin : true,
+};
+
+username.firstname = "Mr. L"
+username.lastname = "Mapui"
+
+// console.log(username.firstname)
+// console.log(username.lastname)
+// console.log(username)
+// console.log(typeof (username));
+
+let today = new Date()
+// console.log(today.getDate());
+
+// Array
+
+// let heros =["a","b", "c", true ]
+
+let anotheruser =["Lokesh", true]
+
+// console.log(anotheruser[0]);
+
+let isvalue = true;
+console.log(isvalue + 1);
---
+++ Hitesh Chowdhary/JS_foundation/part2/primitives.js
@@ -0,0 +1,42 @@
+// Number 
+
+// let balance = 120;
+// console.log(typeof(balance));
+
+// let anotherbalance = new Number(120)
+// console.log(typeof(anotherbalance));
+
+
+// b00lean
+
+// let isactive = true;
+// let isReallyActive = new Boolean(true)
+
+// console.log(isactive);
+
+// Null and Undefined
+
+// let firstname;
+// console.log(firstname);
+
+
+// string
+
+let myString = "Hello";
+let myStringone = "Hola";
+let username = "Lokesh";
+
+let oldGreet =myString + "Lokesh"
+// console.log(oldGreet);
+
+let greetMessage =`Hello ${username}!`; //string interpolation
+let demoone = `Value is ${2*2}`
+// console.log(demoone);
+
+
+//Symbol
+
+let sm1 =Symbol("hitesh")
+let sm2 =Symbol("hitesh")
+
+console.log(sm1 == sm2);
