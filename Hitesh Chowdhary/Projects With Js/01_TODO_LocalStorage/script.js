7a6bdff007a1c7748b25fdadbc643adbf5eee255
Lokesh Mapui <lokeshmapui88@gmail.com>
2026-02-23 19:28:07 +0530

    complete ToDo- LIST app

--- Hitesh Chowdhary/Projects With Js/01_TODO_LocalStorage/script.js
+++ Hitesh Chowdhary/Projects With Js/01_TODO_LocalStorage/script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     addTaskBtn.addEventListener("click", () => {
       const tasktext = todoInput.value.trim();
-      if (tasktext === " ") {
+      if (tasktext === "") {
         return;
       }
       const newTask = {
@@ -18,16 +18,43 @@ document.addEventListener('DOMContentLoaded', () =>{
         completed: false,
       };
       tasks.push(newTask);
+      renderTasks(newTask);
       saveTasks();
-      todoInput.value = " ";
+      todoInput.value = "";
       console.log(tasks);
     });
 
+    todoInput.addEventListener("keypress", function (e) {
+      if (e.key === "Enter") {
+        addTaskBtn.click();
+      }
+    });
+
+
     function renderTasks(task) {
-      console.log(task);
+      const li = document.createElement("li");
+      li.setAttribute("data-id", task.id);
+      if(task.completed) li.classList.add("completed");
+      li.innerHTML = `<span>${task.text}</span>
+      <button class="delete-btn">Delete</button>`;
+
+      li.addEventListener("click", (e) =>{
+        if(e.target.tagName === "BUTTON") return;
+            task.completed = !task.completed;
+            li.classList.toggle("completed");
+            saveTasks();
+      });
+      li.querySelector("button").addEventListener("click", (e) =>{
+        e.stopPropagation();
+        tasks = tasks.filter(t => t.id !== task.id);
+        li.remove();
+        saveTasks();
+      })
+
+      todoList.appendChild(li)
     }
 
     function saveTasks() {
       localStorage.setItem("Tasks", JSON.stringify(tasks));
     }
-})
+});
